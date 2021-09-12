24249522d3aca100c6cc0fc76b3dac9c5b9e76b5
DeannaCarina <deannacarina@hotmail.com>
2021-09-12 22:31:29 +0000

    undo secret key change for local server access

--- neuronet/settings.py
+++ neuronet/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-f6776=7+sel_q#3kdtb%e603)l8q828=n0!$!s+1!kh2&t8(%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['neuronet2021.herokuapp.com', 'localhost']
 
