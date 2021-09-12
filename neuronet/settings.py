2ddb6d6e978248cda3c812e11b6e5c3e15bd86d8
DeannaCarina <deannacarina@hotmail.com>
2021-09-12 23:54:52 +0000

    undo last change

--- neuronet/settings.py
+++ neuronet/settings.py
@@ -166,12 +166,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # cache control
-    AWS_S3_OBJECT_PARAMETERS = (
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT'
-        'CacheControl': 'max-age=94608000',
-    )
-
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'neuronet2021'
     AWS_S3_REGION_NAME = 'eu-west-2'
