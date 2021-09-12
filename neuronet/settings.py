9ab459ae6339e10265a157c7bbfd7a0e3401d50c
Deanna Carina Sale <79478457+DeannaCarina@users.noreply.github.com>
2021-09-12 11:29:34 +0100

    Deployment2 (#86)

--- neuronet/settings.py
+++ neuronet/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f6776=7+sel_q#3kdtb%e603)l8q828=n0!$!s+1!kh2&t8(%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['neuronet2021.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -106,12 +106,18 @@ WSGI_APPLICATION = 'neuronet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
