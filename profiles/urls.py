92ad297ba86e42149f346709ca8330338052dd61
Ryan Thomas <rynthomas@yahoo.com>
2021-09-12 14:38:42 +0000

    Rendered create company profile in create_profile template modal

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name='profiles'),
-    path('create_profile/form_type/', views.create_profile,
+    path('create_profile/<form_type>/', views.create_profile,
          name='create_profile')
 ]
