54814cdcdbce0558252638b4c19636ad9dd6fe3e
Deanna Carina Sale <79478457+DeannaCarina@users.noreply.github.com>
2021-09-13 14:29:39 +0100

    edit databases section in settings.py (#105)

--- neuronet/settings.py
+++ neuronet/settings.py
@@ -110,17 +110,17 @@ WSGI_APPLICATION = 'neuronet.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
