5fdf2a2aa7e37b53d2b37208f6e4250587173e71
DeannaCarina <deannacarina@hotmail.com>
2021-09-13 13:52:21 +0000

    put db settings back

--- neuronet/settings.py
+++ neuronet/settings.py
@@ -110,17 +110,17 @@ WSGI_APPLICATION = 'neuronet.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-# if 'DATABASE_URL' in os.environ:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse('postgres://wrnfrqcdnkqkkr:e724f7349c0d667a2c9985300268403fec4e5bb0200d5fd28f16e93662e1b08c@ec2-54-228-139-34.eu-west-1.compute.amazonaws.com:5432/dqcl1musuf53a')
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
